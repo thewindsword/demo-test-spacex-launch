e7413dedc20c71a329ddea21a7c1b4a5f35962fa
roxas <as5544070@gmail.com>
2022-06-10 19:08:35 +0800

    init: add tailwindcss, react-router;

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,36 @@ import {
   useQuery,
   gql
 } from "@apollo/client";
-import logo from './logo.svg'
+import logoSVG from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const { loading, error, data } = useQuery(GET_LAUNCHES)
   
   console.log('data: ', data);
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {/* <header className="App-header">
+        Hello World
+      </header> */}
+      <div className="relative bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <a href="#">
+                <span className="sr-only">Spacex</span>
+                <img className="h-8 w-auto sm:h-10" src={logoSVG} alt="logo" />
+              </a>
+            </div>
+
+            <nav className="md:flex space-x-10 lg:flex-2">
+              <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900"> Home </a>
+              <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900"> Launches </a>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
